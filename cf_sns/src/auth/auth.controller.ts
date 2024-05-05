f6fca9fbca4dc1f43eeb6b457e30c6389e9134a8
qwe5507 <apvmffkdls@gmail.com>
2024-05-05 19:56:49 +0900

    여러개의 파이프 동시에 사용하기

--- cf_sns/src/auth/auth.controller.ts
+++ cf_sns/src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post, Headers } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { PasswordPipe } from "./pipe/password.pipe";
+import { MaxLengthPipe, MinLengthPipe, PasswordPipe } from "./pipe/password.pipe";
 
 @Controller('auth')
 export class AuthController {
@@ -53,7 +53,7 @@ export class AuthController {
   postRegisterEmail(
     @Body('nickname') nickname: string,
     @Body('email') email: string,
-    @Body('password', PasswordPipe) password: string,
+    @Body('password', new MaxLengthPipe(8), new MinLengthPipe(3)) password: string,
   ) {
     return this.authService.registerWithEmail({
       nickname,
--- cf_sns/src/auth/pipe/password.pipe.ts
+++ cf_sns/src/auth/pipe/password.pipe.ts
@@ -15,3 +15,29 @@ export class PasswordPipe implements PipeTransform {
     return value.toString();
   }
 }
+
+@Injectable()
+export class MaxLengthPipe implements PipeTransform {
+  constructor(private readonly length: number) {}
+
+  transform(value: any, metadata: ArgumentMetadata): any {
+    if (value.toString().length > this.length) {
+      throw new BadRequestException(`최대 길이는 ${this.length}입니다.`);
+    }
+
+    return value.toString();
+  }
+}
+
+@Injectable()
+export class MinLengthPipe implements PipeTransform {
+  constructor(private readonly length: number) {}
+
+  transform(value: any, metadata: ArgumentMetadata): any {
+    if (value.toString().length < this.length) {
+      throw new BadRequestException(`최소 길이는 ${this.length}입니다.`);
+    }
+
+    return value.toString();
+  }
+}
