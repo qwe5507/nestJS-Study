67b3bd88837642747883ebf280027a8fbfb8b8d1
qwe5507 <apvmffkdls@gmail.com>
2024-05-15 19:11:29 +0900

    PickType 활용하기

--- cf_sns/src/posts/entities/posts.entity.ts
+++ cf_sns/src/posts/entities/posts.entity.ts
@@ -1,6 +1,7 @@
 import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { UsersModel } from '../../users/entities/users.entity';
 import { BaseModel } from '../../common/entity/base.entity';
+import { IsString } from 'class-validator';
 
 @Entity()
 export class PostsModel extends BaseModel {
@@ -9,9 +10,15 @@ export class PostsModel extends BaseModel {
   })
   author: UsersModel;
 
+  @IsString({
+    message: 'title은 string 타입을 입력해야 한다',
+  })
   @Column()
   title: string;
 
+  @IsString({
+    message: 'content string 타입을 입력해야 한다',
+  })
   @Column()
   content: string;
 
