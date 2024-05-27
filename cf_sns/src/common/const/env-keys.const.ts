c8ff1a548a35a1773c4d5e531e9b0039cbbf5ce5
qwe5507 <apvmffkdls@gmail.com>
2024-05-27 22:04:08 +0900

    환경변수 추가

---
+++ cf_sns/src/common/const/env-keys.const.ts
@@ -0,0 +1,14 @@
+// 서버 프로토콜 -> http / https
+export const ENV_PROTOCOL_KEY = 'PROTOCOL';
+// 서버 호스트  -> localhost:3000
+export const ENV_HOST_KEY = 'HOST';
+// jwt 토큰 시크릿 -> codefactory
+export const ENV_JWT_SECRET_KEY = 'JWT_SECRET';
+// jwt 토큰 해시 라운드 수 -> 10
+export const ENV_HASH_ROUNDS_KEY = 'HASH_ROUNDS';
+
+export const ENV_DB_HOST_KEY = 'DB_HOST';
+export const ENV_DB_PORT_KEY = 'DB_PORT';
+export const ENV_DB_USERNAME_KEY = 'DB_USERNAME';
+export const ENV_DB_PASSWORD_KEY = 'DB_PASSWORD';
+export const ENV_DB_DATABASE_KEY = 'DB_DATABASE';
