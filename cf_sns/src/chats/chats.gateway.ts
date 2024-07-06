5f0217da1e71b860eaebc81d592f2e5f8c03d660
qwe5507 <apvmffkdls@gmail.com>
2024-07-06 13:57:00 +0900

    Room 활용하기

--- cf_sns/src/chats/chats.gateway.ts
+++ cf_sns/src/chats/chats.gateway.ts
@@ -1,4 +1,5 @@
 import {
+  ConnectedSocket,
   MessageBody,
   OnGatewayConnection,
   SubscribeMessage,
@@ -19,10 +20,28 @@ export class ChatsGateway implements OnGatewayConnection {
     console.log(`on connect called : ${socket.id}`);
   }
 
+  @SubscribeMessage('enter_chat')
+  enterChat(
+    // 방의 ID들을 리스트로 받는다.
+    @MessageBody() data: number[],
+    @ConnectedSocket() socket: Socket,
+  ) {
+    for (const chatId of data) {
+      // socket.join()
+      socket.join(chatId.toString());
+    }
+  }
+
   // socket.on('send_message', (message) => {console.log(message)});
   @SubscribeMessage('send_message')
-  sendMessage(@MessageBody() message: string) {
+  sendMessage(
+    @MessageBody() message: { message: string; chatId: number },
+    @ConnectedSocket() socket: Socket,
+  ) {
     console.log(message);
-    this.server.emit('receive_message', 'hello from server');
+    // this.server.emit('receive_message', 'hello from server');
+    this.server
+      .in(message.chatId.toString()) // 특정 Room에만 전송
+      .emit('receive_message', message.message);
   }
 }
